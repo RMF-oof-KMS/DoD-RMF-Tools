5ae713a184519aab9d2bdefd256d86fe5f24ff2d
RMF-oof-KMS <116330452+RMF-oof-KMS@users.noreply.github.com>
2023-03-10 09:52:55 -0800

    Update Prepare-WinNessusScan.ps1

--- Windows/Nessus Scanning/Prepare-WinNessusScan.ps1
+++ Windows/Nessus Scanning/Prepare-WinNessusScan.ps1
@@ -12,14 +12,14 @@ if ($policy -eq 0) {
     # LocalAccountTokenFilterPolicy is 0, so run the script to set it to 1, enable a user account, and start RemoteRegistry service
     Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "LocalAccountTokenFilterPolicy" -Value 1
     # Enable the user account used for scanning
-    Enable-LocalUser -Name "<username>"
+    Enable-LocalUser -Name $username
     # Start the RemoteRegistry service
     Start-Service -Name "RemoteRegistry"
 } elseif ($policy -eq 1) {
     # LocalAccountTokenFilterPolicy is 1, so run the script to set it to 0, disable a user account, and stop RemoteRegistry service
     Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "LocalAccountTokenFilterPolicy" -Value 0
     # Disable the user account used for scanning
-    Disable-LocalUser -Name "<username>"
+    Disable-LocalUser -Name $username
     Stop-Service -Name "RemoteRegistry"
 } else {
     # LocalAccountTokenFilterPolicy has an invalid value
